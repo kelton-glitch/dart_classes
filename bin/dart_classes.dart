4627c24e83f30d0ccc3ae1dfb8b61c46428fa13d
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-12 22:10:30 -0600

    implementing a class using @override

--- bin/dart_classes.dart
+++ bin/dart_classes.dart
@@ -1,8 +1,18 @@
 //import 'package:dart_classes/dart_classes.dart' as dart_classes;
 
 void main() {
+  final user = new User();
+  user.name = 'Ray';
+  user.id = 40;
+  print(user); //all lcasses are derived from an object
+}
+
   class User {
     int id = 0;
     String name = '';
+    @override
+    String toString() {
+    // TODO: implement toString
+    return 'User(id: $id, name: $name)';
   }
-}
+  }
