7418830218256f9301880342c56832f8f9cd6256
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-13 22:10:30 -0600

    serialization

--- bin/dart_classes.dart
+++ bin/dart_classes.dart
@@ -1,15 +1,19 @@
 //import 'package:dart_classes/dart_classes.dart' as dart_classes;
 
 void main() {
-  final user = new User();
+  final user =  User();
   user.name = 'Ray';
   user.id = 40;
   print(user); //all lcasses are derived from an object
+  print(user.toJson());
 }
 
   class User {
     int id = 0;
     String name = '';
+    String toJson(){
+      return '{"id": $id, "name": $name}';
+    }
     @override
     String toString() {
     // TODO: implement toString
