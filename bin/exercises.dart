1b13c7fe16e2623ec30d2e1011f90791b958666f
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-15 22:10:30 -0600

    exercise 1 and 2

---
+++ bin/exercises.dart
@@ -0,0 +1,11 @@
+// exercise1
+
+class Password{
+  String value = '';
+//exercise2
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Password{value: $value}';
+  }
+}
