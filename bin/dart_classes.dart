90140b1cdde0423a19ecc0ec4e58589677d06726
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-19 22:10:30 -0600

    clas constructors

--- bin/dart_classes.dart
+++ bin/dart_classes.dart
@@ -5,9 +5,26 @@ void main() {
   final user =  User()
   ..name = 'Ray'
   ..id = 40;
+  final person = Person(28, 'Marriage');
+  final anonymousPerson = Person.anonymous();
+  print(anonymousPerson);
+  print(person);
   print(user); //all lcasses are derived from an object
   print(user.toJson());
 }
+
+//short-form constuctor
+class Person{
+Person(this.age, this.aim);
+   int age = 0;
+   String aim ='';
+Person.anonymous (){
+  age = 0;
+  aim = 'anonymous';
+}
+}
+
+//Named constructors
 
   class User {
     int id = 0;
