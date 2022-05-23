c26f195e7000498b96913fcc3166ddaaa212d003
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-05-22 22:10:30 -0600

    implementing named classes

--- bin/dart_classes.dart
+++ bin/dart_classes.dart
@@ -1,30 +1,42 @@
-//import 'package:dart_classes/dart_classes.dart' as dart_classes;
+//import 'package:dart_classes/dart_classes.dart' as dart_class
 
 void main() {
   //using cascade operator
   final user =  User()
   ..name = 'Ray'
   ..id = 40;
-  final person = Person(28, 'Marriage');
+  final person = Person( age : 28, aim: 'Marriage');
   final anonymousPerson = Person.anonymous();
   print(anonymousPerson);
   print(person);
-  print(user); //all lcasses are derived from an object
+  print(user); //all classes are derived from an object
   print(user.toJson());
+
+  final vicki = Person (age: 24, aim: 'Vicki');
+  vicki.aim = 'Nefarious Hacker';
+  print(vicki);
 }
 
 //short-form constuctor
+//edited to named parameter
 class Person{
-Person(this.age, this.aim);
-   int age = 0;
-   String aim ='';
-Person.anonymous (){
-  age = 0;
-  aim = 'anonymous';
-}
+Person({this.age = 0, this.aim = 'anonymous' });
+
+Person.anonymous () : this ();
+   int age ;
+ String aim ;
+
+ @override
+  String toString() {
+    // TODO: implement toString
+    return 'Person(age: $age, aim: $aim)';
+  }
+
 }
 
 //Named constructors
+/// In order to change name to private variable, add _
+/// It makes the library private
 
   class User {
     int id = 0;
